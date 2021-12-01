64e8fbf08da5218460277ba822d73d7875ad9786
mrozim78 <mrozi.m@gmail.com>
2021-12-01 20:52:04 +0100

    Add part2 of solution and some reactor

--- Advent.Solution/Advent.Solution.Day001/Solution.fs
+++ Advent.Solution/Advent.Solution.Day001/Solution.fs
@@ -9,15 +9,37 @@ type Solution() =
         File.ReadAllLines(fileName)
         |> Array.map int
         |> Array.toList
-    let step (state:Option<int>*int,element:int)=
-        let previous , counter = state
-        Some(element), if previous.IsSome && previous.Value<element then counter+1 else counter
+        
+    let stepCountNumberOfLargerThenPrevious (state:Option<int>*int,element:int)=
+        let previousElement, counter = state
+        Some(element), if previousElement.IsSome && previousElement.Value<element then counter+1 else counter
+    
+    let countNumberOfLargerThenPrevious(numbers:int list)=
+        numbers
+        |> List.fold (fun state element -> stepCountNumberOfLargerThenPrevious (state,element)) (Option<int>.None ,0)
+        |> snd
+               
+    let listToTripleSumList(numbers:int list)=
+        seq {0..1..numbers.Length-3}
+        |> Seq.map(fun a ->  numbers.GetSlice(Some(a),None) |> Seq.take 3 |> Seq.sum)
+        |> Seq.toList
+        
+    
+    let solver =
+        let data = readNumbers("../../../../../data/Day001/data.txt")
+        let partOne =
+            data
+            |> countNumberOfLargerThenPrevious
+        let partTwo =
+            data
+            |> listToTripleSumList
+            |> countNumberOfLargerThenPrevious
+        partOne,partTwo
+        
     interface ISolution with
         member this.Solve() =
-            readNumbers("../../../../../data/Day001/data.txt")
-            |> List.fold (fun a b ->  Some(b), if fst(a).IsSome && fst(a).Value<b then snd(a)+1 else snd(a)) (Option<int>.None ,0)
-            |> snd
-            |> string
+           let solveOne , solveTwo = solver
+           $"[ Part 1 value {solveOne} ] [ Part 2 value {solveTwo} ]"
         member this.Day() =
             1
        
