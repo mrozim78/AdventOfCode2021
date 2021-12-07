378ebba9e28ee9c979e550bb7c391ec4bf897779
mrozim78 <mrozi.m@gmail.com>
2021-12-07 18:45:01 +0100

    Change to high order functions

--- Advent.Solution/Advent.Solution.Day007/Solution.fs
+++ Advent.Solution/Advent.Solution.Day007/Solution.fs
@@ -10,36 +10,25 @@ type Solution() =
          |> List.map(fun a->a.Split(",") |> Array.toList)
          |> List.collect(fun a->a)
          |> List.map(fun a->a |> int)
-    let findRow(data:int list, min:int , max:int)=
+
+    let findRowWithFunction(data:int list , min:int,max:int,func:int->int)=
         let lengths =
             seq{min..1..max}
-            |> Seq.map(fun a-> a,data |> List.map(fun b->if a>=b then a-b else b-a) |> List.sum )
+            |> Seq.map(fun a-> a,data |> List.map(fun b->if a>=b then func(a-b) else func(b-a)) |> List.sum )
             
         let min = lengths |> Seq.map(fun a->snd(a)) |> Seq.min
         
         let minLengths = lengths |> Seq.where(fun a-> snd(a)=min)|> Seq.toList
         
         snd(minLengths.[0])
-        
-    let coeff (row:int)=
-        row*(row+1)/2 |> int
-    let findRowWithCoeff(data:int list, min:int , max:int)=
-        let lengths =
-            seq{min..1..max}
-            |> Seq.map(fun a-> a,data |> List.map(fun b->if a>=b then coeff(a-b) else coeff(b-a)) |> List.sum )
-            
-        let min = lengths |> Seq.map(fun a->snd(a)) |> Seq.min
-        
-        let minLengths = lengths |> Seq.where(fun a-> snd(a)=min)|> Seq.toList
-        
-        snd(minLengths.[0])
-    
     let solver =
         let data = readNumbers ("../../../../../data/Day007/data.txt")
         let min = data |> List.min
         let max = data |> List.max
-        let part1 = findRow (data,min,max)
-        let part2 = findRowWithCoeff (data,min,max) |>string 
+        let identity  (x:int) = x
+        let coeff x = x*(x+1)/2 |> int
+        let part1 = findRowWithFunction (data,min,max,identity)
+        let part2 = findRowWithFunction (data,min,max,coeff) 
         part1,part2
     interface ISolution with
          member this.Solve() =
